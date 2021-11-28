38181db61c58f74acc2ec5b90304f2afa06bc979
Leonidasdim <Leondiasdimakis2@gmail.com>
2021-11-28 09:40:26 +0200

    Add project files.

---
+++ Fibonacci/Fibonacci.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Fibonacci
+{
+    class Fibonacci
+    {
+
+        static Dictionary<long, double> Memoization = new Dictionary<long, double>()
+        { { 0,0},{1,1 } };  //
+     
+
+
+        /*Very solw*/
+
+
+        public long  BasicRecrusive(long i )
+        {
+            if (i == 0)
+                return 0;
+            else if (i == 1)
+                return 1;
+            else
+                return BasicRecrusive(i - 1) + BasicRecrusive(i - 2);
+
+        }
+
+
+        /*Memoization*/
+
+        public double MemoizationRecrusive(int i)
+        {
+            if (Memoization.ContainsKey(i))
+                return Memoization[i];
+
+            var value = MemoizationRecrusive(i - 1) + MemoizationRecrusive(i - 2);
+
+            Memoization[i] = value;
+
+            return value;
+
+
+        }
+
+
+
+
+    }
+}
---
+++ Fibonacci/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Fibonacci
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine("Give Fibonacci Sequence number");
+            int number = int.Parse(Console.ReadLine());
+            Fibonacci Seque = new Fibonacci();
+            Stopwatch sw = new Stopwatch();
+
+            sw.Start();
+
+            Console.WriteLine("Fibonaccis is {0}", Seque.MemoizationRecrusive(number));
+            sw.Stop();
+
+            Console.WriteLine("Elapsed={0} seconds", sw.Elapsed.TotalSeconds);
+
+            Console.ReadLine();
+        }
+    }
+}
